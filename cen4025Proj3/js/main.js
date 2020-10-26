05d688d45bf16fda4f22d08b4c578db881bf7513
JeremyDeLancey <35386252+JeremyDeLancey@users.noreply.github.com>
2020-10-26 00:57:09 -0400

    Add files via upload
    -Added JSON of different zoo components and initialized methods
    
    TODO:
    -add html UI components for users to interact
    -finish functions for purchasing ticket, updating zoo animal db, and purchasing gift shop item

---
+++ cen4025Proj3/js/main.js
@@ -0,0 +1,101 @@
+console.log("ready!");
+
+let customers = JSON.parse(`[{
+	"name": "Frank",
+	"hasTicket": true,
+	"contactNumber": "3215558888",
+	"kids": "Jonny, Sally"
+},
+
+{
+	"name": "Jason",
+	"hasTicket": true,
+	"contactNumber": "3215557777",
+	"kids": "Billy"
+},
+
+{
+	"name": "Kati",
+	"hasTicket": true,
+	"contactNumber": "3215554444",
+	"kids": false
+}]`);
+
+let giftShop = JSON.parse(`[
+{
+	"dailyGoal": 5000,
+	"currentTotal": 2000,
+	"isGoalMet": false
+}
+]`);
+
+let employees = JSON.parse(`[
+{
+	"name": "Mary",
+	"role": "employee",
+	"username": "maryJ",
+	"password": "zoo321",
+	"contactNumber": "3215556432"
+},
+
+{
+	"name": "Linda",
+	"role": "management",
+	"username": "LindaJ",
+	"password": "zoo444",
+	"contactNumber": "3215556432"
+},
+
+{
+	"name": "John",
+	"role": "employee",
+	"username": "JohnD",
+	"password": "zoo703",
+	"contactNumber": "3215556432"
+}
+]`);
+
+let animalList = JSON.parse(`[
+{
+	"name": "Bozo",
+	"animalBreed": "monkey",
+	"age": 13
+},
+
+{
+	"name": "Silver",
+	"animalBreed": "zebra",
+	"age": 5
+},
+
+{
+	"name": "Jeff",
+	"animalBreed": "alligator",
+	"age": 46
+}
+]`);
+
+
+//purchasing a ticket
+function purchaseTicket(){
+
+
+}
+
+//purchasing an item from the gift shop
+function purchaseGiftShopItem(){
+
+
+}
+
+function newAnimalArrival(){
+
+	
+}
+
+
+
+
+
+
+
